de7ec0046be177a76d9095dfc43d2cc99ed32c6f
yash-rajpal <rajpal.yash03@gmail.com>
2021-05-08 15:21:16 +0530

    rename app

--- ios/GoLearnTests/GoLearnTests.m
+++ ios/GoLearnTests/GoLearnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SelfyTests : XCTestCase
+@interface GoLearnTests : XCTestCase
 
 @end
 
-@implementation SelfyTests
+@implementation GoLearnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
