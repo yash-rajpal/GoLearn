271f4852c19a27d63e6d68991941dcba6d8cdc8f
Aman-Maheshwari <amanmaheshwari816@gmail.com>
2021-05-17 22:33:18 +0530

    added react-native-image-crop-picker

--- src/components/camera/styles.tsx
+++ src/components/camera/styles.tsx
@@ -1,22 +1,44 @@
-import {StyleSheet} from 'react-native';
-export const style = StyleSheet.create({
+import { StyleSheet, Dimensions } from "react-native";
+import { themes } from "../../constants/colors";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    flexDirection: "column",
+    backgroundColor: "black",
   },
   preview: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   capture: {
     flex: 0,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 35,
     width: 70,
     height: 70,
-    alignSelf: 'center',
+    alignSelf: "center",
     margin: 20,
+  },
+  bottomButtom: {
+    marginTop: "5%",
+    alignSelf: "center",
+    width: WIDTH * 0.35,
+    height: HEIGHT * 0.05,
+    backgroundColor: themes["light"].buttons,
+    borderRadius: 10,
+    justifyContent: "center",
+  },
+  bottomButtonText: {
+    color: themes["light"].buttonText,
+    textAlign: "center",
+  },
+  LoaderAndProgress: {
+    top: HEIGHT / 6,
+    left: WIDTH / 8,
+    position: "absolute",
   },
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import MaterialCommunityIconsI from 'react-native-vector-icons/MaterialCommunityIcons';
-import MaterialIconsI from 'react-native-vector-icons/MaterialIcons';
-import {NavigationContainer} from '@react-navigation/native';
-import {themes} from '../constants/colors';
-import SearchAssignment from '../screens/app/searchAssignment';
-import Dummy from '../screens/dummy';
-import {AuthParamList, MainAppParamList, TabParamList} from './paramLists';
-import ViewAssignment from '../screens/app/viewAssignment';
-import SignUp from '../screens/auth/signup';
-import Camera from '../components/camera';
-import CropEquation from '../components/cropEquation';
-import Auth from '../screens/auth/index';
-import Profile from '../screens/app/Profile';
-import DisplayPDF from '../screens/app/viewAssignment/displayPDF';
-import ReferalScreen from '../screens/app/referalScreen';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIconsI from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIconsI from "react-native-vector-icons/MaterialIcons";
+import { NavigationContainer } from "@react-navigation/native";
+import { themes } from "../constants/colors";
+import SearchAssignment from "../screens/app/searchAssignment";
+import Dummy from "../screens/dummy";
+import { AuthParamList, MainAppParamList, TabParamList } from "./paramLists";
+import ViewAssignment from "../screens/app/viewAssignment";
+import SignUp from "../screens/auth/signup";
+import Camera from "../components/camera";
+import Auth from "../screens/auth/index";
+import Profile from "../screens/app/Profile";
+import DisplayPDF from "../screens/app/viewAssignment/displayPDF";
+import ReferalScreen from "../screens/app/referalScreen";
 
 const AuthStack = createStackNavigator<AuthParamList>();
 const AuthStackNavigator = () => {
   return (
     <AuthStack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName="Auth">
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Auth"
+    >
       <AuthStack.Screen name="SplashScreen" component={Dummy} />
       <AuthStack.Screen name="Auth" component={Auth} />
       <AuthStack.Screen name="SignUp" component={SignUp} />
@@ -35,15 +35,15 @@ const Tab = createBottomTabNavigator<TabParamList>();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused}) => {
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
           let color: string;
-          let iconName: string = 'home';
-          if (route.name === 'Dashboard') {
+          let iconName: string = "home";
+          if (route.name === "Dashboard") {
             color = focused
-              ? themes['light'].activeTintColor
-              : themes['light'].inactiveTintColor;
-            iconName = 'view-dashboard';
+              ? themes["light"].activeTintColor
+              : themes["light"].inactiveTintColor;
+            iconName = "view-dashboard";
             return (
               <MaterialCommunityIconsI
                 name={iconName}
@@ -51,17 +51,17 @@ const TabNavigator = () => {
                 color={color}
               />
             );
-          } else if (route.name === 'Worksheets') {
+          } else if (route.name === "Worksheets") {
             color = focused
-              ? themes['light'].activeTintColor
-              : themes['light'].inactiveTintColor;
-            iconName = 'assignment';
+              ? themes["light"].activeTintColor
+              : themes["light"].inactiveTintColor;
+            iconName = "assignment";
             return <MaterialIconsI name={iconName} size={30} color={color} />;
-          } else if (route.name === 'Profile') {
+          } else if (route.name === "Profile") {
             color = focused
-              ? themes['light'].activeTintColor
-              : themes['light'].inactiveTintColor;
-            iconName = 'account-circle';
+              ? themes["light"].activeTintColor
+              : themes["light"].inactiveTintColor;
+            iconName = "account-circle";
             return (
               <MaterialCommunityIconsI
                 name={iconName}
@@ -76,10 +76,11 @@ const TabNavigator = () => {
         },
       })}
       tabBarOptions={{
-        activeTintColor: themes['light'].activeTintColor,
-        inactiveTintColor: themes['light'].inactiveTintColor,
+        activeTintColor: themes["light"].activeTintColor,
+        inactiveTintColor: themes["light"].inactiveTintColor,
         keyboardHidesTabBar: true,
-      }}>
+      }}
+    >
       <Tab.Screen name="Dashboard" component={Dummy} />
       <Tab.Screen name="Worksheets" component={SearchAssignment} />
       <Tab.Screen name="Profile" component={Profile} />
@@ -93,15 +94,15 @@ const MainAppStackNavigator = () => {
   return (
     <NavigationContainer>
       <MainAppStack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="AuthFlow">
+        screenOptions={{ headerShown: false }}
+        initialRouteName="AuthFlow"
+      >
         <MainAppStack.Screen name="AuthFlow" component={AuthStackNavigator} />
         <MainAppStack.Screen name="AppFlow" component={TabNavigator} />
         <MainAppStack.Screen name="ViewAssignment" component={ViewAssignment} />
         <MainAppStack.Screen name="DisplayPDF" component={DisplayPDF} />
         <MainAppStack.Screen name="Dummy" component={Dummy} />
         <MainAppStack.Screen name="Camera" component={Camera} />
-        <MainAppStack.Screen name="CropEquation" component={CropEquation} />
         <MainAppStack.Screen name="ReferalScreen" component={ReferalScreen} />
       </MainAppStack.Navigator>
     </NavigationContainer>
